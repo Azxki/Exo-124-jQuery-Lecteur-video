973783f10acfb016ff767afdbea1e4d3f04cdf4f
azxki <cococoursan@gmail.com>
2022-12-20 16:34:38 +0100

    exo

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
 // FIXME Assurez vous que ces deux variables contiennent bien les bonnes choses ;-)
-let player = $('.player__video').first();
-let progress = $('.progress__filled').first();
+let player = $('.player__video');
+let progress = $('.progress__filled');
+let play_but = $('.toggle');
+let player__slide = $('.player__slider');
+
+play_but.click(() => {
+    player[0].play();
+    play_but.html('\u23f8');
+    play_but.click(() => {
+        player[0].pause();
+        play_but.html('►');
+    })
+})
